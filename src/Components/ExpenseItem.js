a2b06ff8fb2f0564b62e52289ad878ea744db99e
Ddas2100 <ddas2100@gmail.com>
2023-09-20 22:15:46 +0530

    ReactModule1_Task7(ExpenseItems)

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <h4 className="inline">Food</h4><div className="inline">Rs. 10</div>
+            </div>
+            <div>
+                <h4 className="inline">Petrol</h4><div className="inline">Rs. 100</div>
+            </div>
+            <div>
+                <h4 className="inline">Movies</h4><div className="inline">Rs. 200</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
