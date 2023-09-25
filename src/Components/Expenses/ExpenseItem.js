3d1ad36b067518493fe70254f9a85f16074b3f9c
Ddas2100 <ddas2100@gmail.com>
2023-09-25 23:28:37 +0530

    Module2(Delete_Button)_Task1

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -6,22 +6,25 @@ import './ExpenseItem.css';
 import './Expenses.css'
 
 const ExpenseItem=(props) => {
+    const clickHandler= () => {
+        console.log('clicked!!!')
+    }
     return (
+        // <Card className= "expense-item">
+        //     <ExpenseDate date={props.date} />
+        //     <ExpenseDetails className='expense-item__description'
+        //         title={props.title} amount= {props.amount} LocationOfExpenditure={props.LocationOfExpenditure}>
+        //     </ExpenseDetails>
+        // </Card>
         <Card className= "expense-item">
             <ExpenseDate date={props.date} />
-            <ExpenseDetails className='expense-item__description'
-                title={props.title} amount= {props.amount} LocationOfExpenditure={props.LocationOfExpenditure}>
-            </ExpenseDetails>
+            <div className='expense-item__description'>
+                <h3>{props.title}</h3>
+                <div className='expense-item__price'>${props.amount}</div>
+                {/* <h4>{props.LocationOfExpenditure}</h4> */}
+            </div>
+            <button className='deleteButton' onClick={clickHandler}>Delete</button>
         </Card>
-        //     <ExpenseDetails className='expense-item__description'
-        //         title={props.title} amount= {props.amount} LocationOfExpenditure={props.LocationOfExpenditure} 
-        //     />
-        //     <div className='expense-item__description'>
-        //         <h3>{props.title}</h3>
-        //         <div className='expense-item__price'>${props.amount}</div>
-        //         <div>{props.LocationOfExpenditure}</div>
-        //     </div>
-        // </Card>
     ); 
 }
 
