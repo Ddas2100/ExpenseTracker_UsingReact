b63467bedd156ba992daa2f7be3dacd0b32d90f9
Ddas2100 <ddas2100@gmail.com>
2023-10-02 22:08:53 +0530

    AdvReact_Chart App-Task 14

--- src/App.js
+++ src/App.js
@@ -14,19 +14,25 @@ const DUMMY_EXPENSES = [
     id: 'e2',
     title: 'New TV',
     amount: 799.49,
-    date: new Date(2023, 7, 15),
+    date: new Date(2022, 5, 15),
   },
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 294.67,
-    date: new Date(2021, 7, 16),
+    date: new Date(2021, 9, 16),
   },
   {
     id: 'e4',
     title: 'New Desk',
     amount: 450,
     date: new Date(2022, 7, 18),
+  },
+  {
+    id: 'e5',
+    title: 'Television',
+    amount: 299,
+    date: new Date(2023, 10, 18),
   }
 ]
 
